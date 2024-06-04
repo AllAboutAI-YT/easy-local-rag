4dae9e017bd9c757317f82f0ea214716dc779a50
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2024-06-04 19:32:47 +0200

    Update upload.py
    - added some logging, with time elapsed
    - change "\n" for u"\n"

--- upload.py
+++ upload.py
@@ -1,14 +1,28 @@
+import logging
+# Configurer le logging // pip install logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger.info("START")
+logger.debug("Import Modules")
 import os
 import tkinter as tk
 from tkinter import filedialog
 import PyPDF2
 import re
 import json
+import time
+logger.debug("Import Modules : Done.")
 
 # Function to convert PDF to text and append to vault.txt
 def convert_pdf_to_text():
     file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
     if file_path:
+        logger.info(f'Received File : {file_path}')
+        logger.info('Please wait...')
+        tic = time.perf_counter()
         with open(file_path, 'rb') as pdf_file:
             pdf_reader = PyPDF2.PdfReader(pdf_file)
             num_pages = len(pdf_reader.pages)
@@ -38,15 +52,19 @@ def convert_pdf_to_text():
             with open("vault.txt", "a", encoding="utf-8") as vault_file:
                 for chunk in chunks:
                     # Write each chunk to its own line
-                    vault_file.write(chunk.strip() + "\n")  # Two newlines to separate chunks
-            print(f"PDF content appended to vault.txt with each chunk on a separate line.")
+                    vault_file.write(chunk.strip() + u"\n")  # Two newlines to separate chunks
+            logger.info(f"PDF content appended to vault.txt with each chunk on a separate line.")
+        toc = time.perf_counter()
+        logger.info(f"Duration : {toc - tic:0.4f} seconds")
 
+logger.debug("Setting Functions")
 # Function to upload a text file and append to vault.txt
 def upload_txtfile():
     file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
     if file_path:
-        print(f'Received File : {file_path}')
-        print('Please wait...')
+        logger.info(f'Received File : {file_path}')
+        logger.info('Please wait...')
+        tic = time.perf_counter()
         with open(file_path, 'r', encoding="utf-8") as txt_file:
             text = txt_file.read()
             
@@ -70,13 +88,18 @@ def upload_txtfile():
             with open("vault.txt", "a", encoding="utf-8") as vault_file:
                 for chunk in chunks:
                     # Write each chunk to its own line
-                    vault_file.write(chunk.strip() + "\n")  # Two newlines to separate chunks
-            print(f"Text file content appended to vault.txt with each chunk on a separate line.")
+                    vault_file.write(chunk.strip() + u"\n")  # Two newlines to separate chunks
+            logger.info(f"Text file content appended to vault.txt with each chunk on a separate line.")
+        toc = time.perf_counter()
+        logger.info(f"Duration : {toc - tic:0.4f} seconds")
 
 # Function to upload a JSON file and append to vault.txt
 def upload_jsonfile():
     file_path = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
     if file_path:
+        logger.info(f'Received File : {file_path}')
+        logger.info('Please wait...')
+        tic = time.perf_counter()
         with open(file_path, 'r', encoding="utf-8") as json_file:
             data = json.load(json_file)
             
@@ -103,24 +126,28 @@ def upload_jsonfile():
             with open("vault.txt", "a", encoding="utf-8") as vault_file:
                 for chunk in chunks:
                     # Write each chunk to its own line
-                    vault_file.write(chunk.strip() + "\n")  # Two newlines to separate chunks
-            print(f"JSON file content appended to vault.txt with each chunk on a separate line.")
+                    vault_file.write(chunk.strip() + u"\n")  # Two newlines to separate chunks
+            logger.info(f"JSON file content appended to vault.txt with each chunk on a separate line.")
+        toc = time.perf_counter()
+        logger.info(f"Duration : {toc - tic:0.4f} seconds")
 
-# Create the main window
+logger.debug("Setting Functions : Done.")
+
+logger.debug("Create the main window")
 root = tk.Tk()
 root.title("Upload .pdf, .txt, or .json")
 
-# Create a button to open the file dialog for PDF
+logger.debug("Create a button to open the file dialog for PDF")
 pdf_button = tk.Button(root, text="Upload PDF", command=convert_pdf_to_text)
 pdf_button.pack(pady=10)
 
-# Create a button to open the file dialog for text file
+logger.debug("Create a button to open the file dialog for TXT")
 txt_button = tk.Button(root, text="Upload Text File", command=upload_txtfile)
 txt_button.pack(pady=10)
 
-# Create a button to open the file dialog for JSON file
+logger.debug("Create a button to open the file dialog for JSON")
 json_button = tk.Button(root, text="Upload JSON File", command=upload_jsonfile)
 json_button.pack(pady=10)
 
-# Run the main event loop
+logger.debug("Run the main event loop")
 root.mainloop()
